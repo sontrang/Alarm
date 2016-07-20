4d9ffe1fd1aabec8e7c92bd069ca580ccd4aa9ec
Trang To <tothitrang2ptit@gmail.com>
2016-07-20 11:56:03 +0700

    test

--- app/src/main/java/poiuyt/alarm/activities/AlarmOffActivity.java
+++ app/src/main/java/poiuyt/alarm/activities/AlarmOffActivity.java
@@ -3,16 +3,11 @@ package poiuyt.alarm.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
-
 import java.io.Serializable;
 
 import poiuyt.alarm.R;
 
 public class AlarmOffActivity extends Activity{
-
-
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
